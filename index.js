7705c108302fa873e9a23e1d3a3743388e45546d
kamal-singh <kamal000rawat@gmail.com>
2022-02-25 12:09:04 +0530

    25-02-22 first commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
@@ -11,7 +12,6 @@ io.on('connection', (socket) => {
     console.log('connection established');
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
-        console.log(users);
         socket.broadcast.emit('user-joined', name);
     });
     socket.on('send', (message) => {
@@ -20,6 +20,8 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('disconnect connection');
+        socket.broadcast.emit('leave', users[socket.id]);
+        delete users[socket.id];
     })
 })
 
